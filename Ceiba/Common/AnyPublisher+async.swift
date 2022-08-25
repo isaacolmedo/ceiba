81650dea886f2675a37a0cc803bdf7eccd72361a
isaac.olmedo <olmedoisaac31@gmail.com>
2022-08-25 11:48:19 -0500

    [Feat] Add extension to AnyPublisher to convert to async

---
+++ Ceiba/Common/AnyPublisher+async.swift
@@ -0,0 +1,30 @@
+//
+//  Cancellable.swift
+//  Ceiba
+//
+//  Created by Isaac Olmedo on 25/08/22.
+//
+
+import Foundation
+import Combine
+
+extension AnyPublisher {
+    func async() async throws -> Output {
+        try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
+            
+            cancellable = first()
+                .sink { result in
+                    switch result {
+                    case .finished:
+                        break
+                    case let .failure(error):
+                        continuation.resume(throwing: error)
+                    }
+                    cancellable?.cancel()
+                } receiveValue: { value in
+                    continuation.resume(with: .success(value))
+                }
+        }
+    }
+}
